7bd1eabb169092ff93641977b1b793f4403b5e9d
Scott Mikula <mikula@gmail.com>
2017-04-27 16:35:34 -0700

    Add test case for RequestCount

--- FetchOBotTests/TrackOBotTests.cs
+++ FetchOBotTests/TrackOBotTests.cs
@@ -7,24 +7,35 @@
     [TestClass]
     public class TrackOBotTests
     {
+        private const string ResponseJson =
+            @"{
+                ""history"" : [{}, {}],
+                ""meta"": {}
+            }";
+
         [TestMethod]
         public void TestGetHistoryPageAsync()
         {
-            var webClient = new MockWebClient
-            {
-                ResponseString =
-                    @"{
-                        ""history"" : [{}, {}],
-                        ""meta"": {}
-                    }"
-            };
-
+            var webClient = new MockWebClient { ResponseString = ResponseJson };
             var trackOBot = new TrackOBot(webClient);
             var result = trackOBot.GetHistoryPageAsync("testUsername", "testApiToken", 2).Result;
 
             Assert.AreEqual("https://trackobot.com/profile/history.json?username=testUsername&token=testApiToken&page=2", webClient.LastUrl);
             Assert.AreEqual(2, result.History.Length);
             Assert.IsNotNull(result.Meta);
+        }
+
+        [TestMethod]
+        public void TestRequestCount()
+        {
+            var webClient = new MockWebClient { ResponseString = ResponseJson };
+            var trackOBot = new TrackOBot(webClient);
+
+            Assert.AreEqual(0, trackOBot.RequestCount);
+            trackOBot.GetHistoryPageAsync("testUsername", "testApiToken", 2).Wait();
+            trackOBot.GetHistoryPageAsync("testUsername", "testApiToken", 2).Wait();
+
+            Assert.AreEqual(2, trackOBot.RequestCount);
         }
 
         [TestMethod]
