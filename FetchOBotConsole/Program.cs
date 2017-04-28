132899651ac4f49b74ff254bc1086528ace1bc4e
Scott Mikula <mikula@gmail.com>
2017-04-27 20:24:13 -0700

    Add FetchOBotConsole app for testing purposes

---
+++ FetchOBotConsole/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace FetchOBotConsole
+{
+    using System;
+    using FetchOBotApi;
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Username: ");
+                var username = Console.ReadLine();
+
+                Console.Write("API token: ");
+                var apiToken = Console.ReadLine();
+
+                var fetchOBot = new FetchOBot();
+                var history = fetchOBot.GetHistoryRangeAsync(username, apiToken, DateTime.UtcNow.AddDays(-14)).Result;
+                Console.WriteLine("Done.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }
+        }
+    }
+}
